9f9b5ac3731d389d8e8a97811035e5343be0688d
Ghouse Basha Shaik <ghousebashashaik@Apples-MacBook-Pro.local>
2018-06-10 22:06:17 +0530

    auto resize of the textview

--- AutoResizeTextField/ViewController.swift
+++ AutoResizeTextField/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let textView = UITextView.init(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        textView.backgroundColor = UIColor.gray
+        view.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.preferredFont(forTextStyle: .headline)
+        [
+            textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            textView.heightAnchor.constraint(equalToConstant: 50)
+        ].forEach({$0.isActive = true})
+        textView.isScrollEnabled = false
+        textView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let completeSize = CGSize(width: view.frame.width, height: .infinity)
+        let estimationSize = textView.sizeThatFits(completeSize)
+        textView.constraints.forEach {(constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimationSize.height
+            }
+        }
+    }
+}
